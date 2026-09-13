92e408f1a7851fb0960ed15795b1787136eb65b3
tupotapok <80213531+tupotapok@users.noreply.github.com>
2026-09-13 21:07:53 +0300

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,16 +1,10 @@
 import json
-import os
 
-# Проверяем, существует ли файл
-if not os.path.exists('adult.lst'):
-    print("Файл adult.lst не найден!")
-    exit(1)
-
-# Читаем домены
-with open('adult.lst', 'r') as f:
+# Чтение итогового списка доменов
+with open('adult.lst', 'r', encoding='utf-8') as f:
     domains = [line.strip() for line in f if line.strip() and not line.startswith('#')]
 
-# Формируем структуру правила для sing-box
+# Формирование структуры JSON для sing-box
 rule_set = {
     "version": 1,
     "rules": [
@@ -20,6 +14,6 @@ rule_set = {
     ]
 }
 
-# Сохраняем в JSON
-with open('adult.json', 'w') as f:
+# Сохранение в adult.json
+with open('adult.json', 'w', encoding='utf-8') as f:
     json.dump(rule_set, f, indent=2)
